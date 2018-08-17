d668ec274beeb000e0e916c7040cae779e242fec
adams85 <adamosimoni@gmail.com>
2018-08-17 14:24:06 +0200

    workaround for https://github.com/Microsoft/msbuild/issues/2221

---
+++ test/Karambolo.PO.Test/Properties/Resources.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Karambolo.PO.Test.Properties
+{
+    public static class Resources
+    {
+        static readonly Assembly assembly = typeof(Resources).Assembly;
+
+        public static byte[] SamplePO
+        {
+            get
+            {
+                using (var stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.Resources.sample.po"))
+                using (var ms = new MemoryStream((int)stream.Length))
+                {
+                    stream.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
+        }
+    }
+}
