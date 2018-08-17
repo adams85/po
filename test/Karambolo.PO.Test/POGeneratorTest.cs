5cd64b520920341350a78ecd4681740a7ad295f6
adams85 <adamosimoni@gmail.com>
2018-08-17 13:59:12 +0200

    essential tests added

---
+++ test/Karambolo.PO.Test/POGeneratorTest.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using Karambolo.Common.Collections;
+using Karambolo.PO.Test.Properties;
+using Xunit;
+
+namespace Karambolo.PO.Test
+{
+    public class POGeneratorTest
+    {
+        static readonly POCatalog catalog = CreateCatalog();
+
+        public static POCatalog CreateCatalog()
+        {
+            var result = new POCatalog();
+
+            result.HeaderComments = new POComment[]
+            {
+                new POTranslatorComment { Text = "header comment" }
+            };
+
+            result.Headers = new OrderedDictionary<string, string>
+            {
+                { "Language-Team", "" },
+                { "PO-Revision-Date", "" },
+                { "POT-Creation-Date", "2018-06-22 07:01+0200" },
+                { "Project-Id-Version", "" },
+                { "Report-Msgid-Bugs-To", "" },
+                { "MIME-Version", "1.0" },
+                { "X-Generator", "Poedit 2.0.8" },
+                { "Last-Translator", "" },
+            };
+
+            result.Encoding = "UTF-8";
+            result.PluralFormCount = 2;
+            result.PluralFormSelector = "(n != 1)";
+            result.Language = "en_US";
+
+            var key = new POKey("{0} hour to midnight", "{0} hours to midnight", "Home");
+            IPOEntry entry = new POPluralEntry(key)
+            {
+                "Translation of {0} hour to midnight",
+                "Translation of {0} hours to midnight",
+            };
+            entry.Comments = new POComment[]
+            {
+                new POTranslatorComment { Text = "some translator comment" },
+                new POExtractedComment { Text = "some extracted comment" },
+                new POReferenceComment { References = new POSourceReference[] { new POSourceReference("/Views/Home/Index.cshtml", 8) } },
+                new POFlagsComment { Flags = new SortedSet<string> { "fuzzy", "csharp-format" } },
+                new POPreviousValueComment { IdKind = POIdKind.Id, Value = "{0} hour to midnite" },
+                new POPreviousValueComment { IdKind = POIdKind.PluralId, Value = "{0} hours to midnite" },
+            };
+            result.Add(entry);
+
+            key = new POKey($"Here is an example of how one might continue a very long string{Environment.NewLine}" +
+                $"for the common case the string represents multi-line output.{Environment.NewLine}");
+            entry = new POSingularEntry(key)
+            {
+                Translation = "Some translation of long text"
+            };
+            result.Add(entry);
+
+            return result;
+        }
+
+        [Fact]
+        public void GenerateFull()
+        {
+            var generator = new POGenerator();
+
+            string result;
+            using (var ms = new MemoryStream())
+            {
+                generator.Generate(ms, catalog);
+                result = Encoding.UTF8.GetString(ms.ToArray());
+            }
+
+            // Encoding.GetString keeps BOM
+            var expected = new StreamReader(new MemoryStream(Resources.SamplePO)).ReadToEnd()
+                .Replace("# should be skipped\r\n", "")
+                .Replace("# should be skipped too\r\n", "");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GenerateSkipComments()
+        {
+            var generator = new POGenerator(new POGeneratorSettings { SkipComments = true });
+
+            string result;
+            using (var ms = new MemoryStream())
+            {
+                generator.Generate(ms, catalog);
+                result = Encoding.UTF8.GetString(ms.ToArray());
+            }
+
+            // Encoding.GetString keeps BOM
+            var expected = new StreamReader(new MemoryStream(Resources.SamplePO)).ReadToEnd();
+            expected = Regex.Replace(expected, @"(^|\r\n)(#[^\r]*\r\n)+", "$1");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GenerateSkipInfoHeaders()
+        {
+            var generator = new POGenerator(new POGeneratorSettings { SkipInfoHeaders = true });
+
+            string result;
+            using (var ms = new MemoryStream())
+            {
+                generator.Generate(ms, catalog);
+                result = Encoding.UTF8.GetString(ms.ToArray());
+            }
+
+            // Encoding.GetString keeps BOM
+            var expected = new StreamReader(new MemoryStream(Resources.SamplePO)).ReadToEnd()
+                .Replace("\"Language-Team: \\n\"\r\n", "")
+                .Replace("\"Last-Translator: \\n\"\r\n", "")
+                .Replace("\"MIME-Version: 1.0\\n\"\r\n", "")
+                .Replace("\"PO-Revision-Date: \\n\"\r\n", "")
+                .Replace("\"POT-Creation-Date: 2018-06-22 07:01+0200\\n\"\r\n", "")
+                .Replace("\"Project-Id-Version: \\n\"\r\n", "")
+                .Replace("\"Report-Msgid-Bugs-To: \\n\"\r\n", "")
+                .Replace("\"X-Generator: Poedit 2.0.8\\n\"\r\n", "")
+                .Replace("# should be skipped\r\n", "")
+                .Replace("# should be skipped too\r\n", "");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ test/Karambolo.PO.Test/POParserTest.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Karambolo.PO.Test.Properties;
+using Xunit;
+
+namespace Karambolo.PO.Test
+{
+    public class POParserTest
+    {
+        void CheckHeader(POCatalog catalog, bool expectComments, bool expectInfoHeaders)
+        {
+            if (expectInfoHeaders)
+            {
+                Assert.Equal(12, catalog.Headers.Count);
+
+                Assert.Equal("8bit", catalog.Headers["Content-Transfer-Encoding"]);
+                Assert.Equal("text/plain; charset=UTF-8", catalog.Headers["Content-Type"]);
+                Assert.Equal("", catalog.Headers["Language-Team"]);
+                Assert.Equal("nplurals=2; plural=(n != 1);", catalog.Headers["Plural-Forms"]);
+                Assert.Equal("", catalog.Headers["PO-Revision-Date"]);
+                Assert.Equal("2018-06-22 07:01+0200", catalog.Headers["POT-Creation-Date"]);
+                Assert.Equal("", catalog.Headers["Project-Id-Version"]);
+                Assert.Equal("", catalog.Headers["Report-Msgid-Bugs-To"]);
+                Assert.Equal("1.0", catalog.Headers["MIME-Version"]);
+                Assert.Equal("Poedit 2.0.8", catalog.Headers["X-Generator"]);
+                Assert.Equal("", catalog.Headers["Last-Translator"]);
+                Assert.Equal("en_US", catalog.Headers["Language"]);
+            }
+            else
+                Assert.Null(catalog.Headers);
+
+            Assert.Equal("UTF-8", catalog.Encoding);
+            Assert.Equal("en_US", catalog.Language);
+            Assert.Equal(2, catalog.PluralFormCount);
+            Assert.Equal("(n != 1)", catalog.PluralFormSelector);
+            Assert.Equal(0, catalog.GetPluralFormIndex(1));
+            Assert.Equal(1, catalog.GetPluralFormIndex(2));
+            Assert.Equal(1, catalog.GetPluralFormIndex(5));
+
+            if (expectComments)
+            {
+                Assert.Equal(1, catalog.HeaderComments.Count);
+
+                var comments = catalog.HeaderComments;
+                Assert.Equal(POCommentKind.Translator, comments[0].Kind);
+                Assert.Equal("header comment", ((POTranslatorComment)comments[0]).Text);
+            }
+            else
+                Assert.Null(catalog.HeaderComments);
+        }
+
+        void CheckItems(POCatalog catalog, bool expectComments)
+        {
+            var key1 = new POKey("{0} hour to midnight", "{0} hours to midnight", "Home");
+            var key2 = new POKey($"Here is an example of how one might continue a very long string{Environment.NewLine}" +
+                $"for the common case the string represents multi-line output.{Environment.NewLine}");
+
+            Assert.Equal(2, catalog.Count);
+            Assert.Contains(key1, catalog.Keys);
+            Assert.Contains(key2, catalog.Keys);
+
+            Assert.Equal(2, catalog[key1].Count);
+            Assert.Equal("Translation of {0} hour to midnight", catalog.GetTranslation(key1, 1));
+            Assert.Equal("Translation of {0} hours to midnight", catalog[key1][1]);
+            Assert.Equal("Translation of {0} hour to midnight", catalog.GetTranslation(key1, 1));
+            Assert.Equal("Translation of {0} hours to midnight", catalog.GetTranslation(key1, 2));
+
+            Assert.Equal(1, catalog[key2].Count);
+            Assert.Equal("Some translation of long text", catalog[key2][0]);
+
+            if (expectComments)
+            {
+                Assert.Equal(6, catalog[key1].Comments.Count);
+                Assert.Equal(0, catalog[key2].Comments.Count);
+
+                var comments = catalog[key1].Comments;
+
+                Assert.Equal(POCommentKind.Translator, comments[0].Kind);
+                Assert.Equal("some translator comment", ((POTranslatorComment)comments[0]).Text);
+
+                Assert.Equal(POCommentKind.Extracted, comments[1].Kind);
+                Assert.Equal("some extracted comment", ((POExtractedComment)comments[1]).Text);
+
+                Assert.Equal(POCommentKind.Reference, comments[2].Kind);
+                var references = ((POReferenceComment)comments[2]).References;
+                Assert.Equal(1, references.Count);
+                Assert.Equal("/Views/Home/Index.cshtml", references[0].FilePath);
+                Assert.Equal(8, references[0].Line);
+
+                Assert.Equal(POCommentKind.Flags, comments[3].Kind);
+                var flags = ((POFlagsComment)comments[3]).Flags;
+                Assert.Equal(2, flags.Count);
+                Assert.Contains("fuzzy", flags);
+                Assert.Contains("csharp-format", flags);
+
+                Assert.Equal(POCommentKind.PreviousValue, comments[4].Kind);
+                Assert.Equal(POIdKind.Id, ((POPreviousValueComment)comments[4]).IdKind);
+                Assert.Equal("{0} hour to midnite", ((POPreviousValueComment)comments[4]).Value);
+
+                Assert.Equal(POCommentKind.PreviousValue, comments[5].Kind);
+                Assert.Equal(POIdKind.PluralId, ((POPreviousValueComment)comments[5]).IdKind);
+                Assert.Equal("{0} hours to midnite", ((POPreviousValueComment)comments[5]).Value);
+            }
+            else
+            {
+                Assert.Null(catalog[key1].Comments);
+                Assert.Null(catalog[key2].Comments);
+            }
+        }
+
+        [Fact]
+        public void ParseFull()
+        {
+            var parser = new POParser();
+
+            POParseResult result;
+            using (var ms = new MemoryStream(Resources.SamplePO))
+                result = parser.Parse(ms);
+
+            Assert.True(result.Success);
+
+            var catalog = result.Catalog;
+            CheckHeader(catalog, expectComments: true, expectInfoHeaders: true);
+            CheckItems(catalog, expectComments: true);
+        }
+
+        [Fact]
+        public void ParseHeaderOnly()
+        {
+            var parser = new POParser(new POParserSettings
+            {
+                ReadHeaderOnly = true
+            });
+
+            POParseResult result;
+            using (var ms = new MemoryStream(Resources.SamplePO))
+                result = parser.Parse(ms);
+
+            Assert.True(result.Success);
+
+            var catalog = result.Catalog;
+            CheckHeader(catalog, expectComments: true, expectInfoHeaders: true);
+            Assert.Empty(catalog);
+        }
+
+        [Fact]
+        public void ParseSkipComments()
+        {
+            var parser = new POParser(new POParserSettings
+            {
+                SkipComments = true
+            });
+
+            // Encoding.GetString keeps BOM
+            var input = new StreamReader(new MemoryStream(Resources.SamplePO)).ReadToEnd();
+            var result = parser.Parse(input);
+
+            Assert.True(result.Success);
+
+            var catalog = result.Catalog;
+            CheckHeader(catalog, expectComments: false, expectInfoHeaders: true);
+            CheckItems(catalog, expectComments: false);
+        }
+
+        [Fact]
+        public void ParseSkipInfoHeaders()
+        {
+            var parser = new POParser(new POParserSettings
+            {
+                SkipInfoHeaders = true
+            });
+
+            var input = new StreamReader(new MemoryStream(Resources.SamplePO));
+            var result = parser.Parse(input);
+
+            Assert.True(result.Success);
+
+            var catalog = result.Catalog;
+            CheckHeader(catalog, expectComments: true, expectInfoHeaders: false);
+            CheckItems(catalog, expectComments: true);
+        }
+    }
+}
