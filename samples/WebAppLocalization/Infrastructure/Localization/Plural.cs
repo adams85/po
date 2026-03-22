c07014271a39597f7408471e3d4108c0e862e4a8
Adam Simon <adamosimoni@gmail.com>
2026-03-23 00:28:23 +0100

    Update sample app

---
+++ samples/WebAppLocalization/Infrastructure/Localization/Plural.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Karambolo.Common.Localization
+{
+    public readonly struct Plural : IFormattable
+    {
+        public static Plural From(string id, int count)
+        {
+            return new Plural(id, count);
+        }
+
+        private Plural(string id, int count)
+        {
+            if (id == null)
+                throw new ArgumentNullException(nameof(id));
+
+            Id = id;
+            Count = count;
+        }
+
+        public string Id { get; }
+        public int Count { get; }
+
+        public override string ToString()
+        {
+            return Count.ToString();
+        }
+
+        public string ToString(string? format, IFormatProvider? formatProvider)
+        {
+            return Count.ToString(format, formatProvider);
+        }
+    }
+}
---
+++ samples/WebAppLocalization/Infrastructure/Localization/TextContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Karambolo.Common.Localization
+{
+    public readonly struct TextContext
+    {
+        public static TextContext From(string id)
+        {
+            return new TextContext(id);
+        }
+
+        private TextContext(string id)
+        {
+            if (id == null)
+                throw new ArgumentNullException(nameof(id));
+
+            Id = id;
+        }
+
+        public string Id { get; }
+    }
+}
